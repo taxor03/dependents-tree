6e40efdd44fa647daff6cf343f898a28c8bc10f1
taxor03 <ryan040@korea.ac.kr>
2024-11-05 16:43:32 +0900

    refactoring leaf node

--- dependents.py
+++ dependents.py
@@ -2,8 +2,6 @@ import requests
 
 # Recursive function to fetch dependents and build a tree structure
 def fetch_dependents(package, version, current_depth, depth_limit):
-    if current_depth > depth_limit:
-        return {}  # Stop if depth limit is reached
 
     # Construct the API URL, handling scoped packages (starting with '@')
     if package.startswith('@'):
@@ -34,27 +32,38 @@ def fetch_dependents(package, version, current_depth, depth_limit):
     # Fetch the direct samples (dependents)
 
     # Recursively build the dependency tree for each dependent
+    i = 1
     for sample in direct_sample:
         dep_name = sample['package']['name']
         dep_version = sample['version']
         
         # Recursive call to get the dependents of the current dependent
-        dependent_tree = fetch_dependents(dep_name, dep_version, current_depth + 1, depth_limit)
-        dependents = dependent_tree.get("dependents", {})
-        # Add the dependent package information to the tree
-        tree["dependents"].append({
-            "package": dep_name,
-            "version": dep_version,
-            "dep_nums" : len(dependents),
-            "dependents": dependents  # Attach the dependents subtree
-        })
+        if current_depth < depth_limit:
+            dependent_tree = fetch_dependents(dep_name, dep_version, current_depth + 1, depth_limit)
+            dependents = dependent_tree.get("dependents", {})
+            # Add the dependent package information to the tree
+            tree["dependents"].append({
+                "no": i,
+                "package": dep_name,
+                "version": dep_version,
+                "dep_nums" : len(dependents),
+                "dependents": dependents  # Attach the dependents subtree
+            })
+        else:
+            tree["dependents"].append({
+                "no": i,
+                "package": dep_name,
+                "version": dep_version,
+            })
+        i += 1
+            
     
     return tree
 
 # Initial parameters
 root_package = "lodash.merge"
 root_version = "4.6.2"
-depth_limit = 1  # Set the maximum recursion depth
+depth_limit = 1   # Set the maximum recursion depth
 
 # Start fetching and building the dependency tree from the root package
 dependency_tree = fetch_dependents(root_package, root_version, 1, depth_limit)
